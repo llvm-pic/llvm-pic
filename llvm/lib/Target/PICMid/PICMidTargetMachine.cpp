7a40f43c0a2e52881c7c6485633930cbc178cf17
Thanh <quang.thanh.ta@pr1meti.de>
2023-10-07 20:41:11 +0200

    PICMid: Create scaffold for new PICMid target

---
+++ llvm/lib/Target/PICMid/PICMidTargetMachine.cpp
@@ -0,0 +1,15 @@
+#include "llvm/BinaryFormat/ELF.h"
+#include "llvm/MC/MCAsmBackend.h"
+#include "llvm/MC/MCCodeEmitter.h"
+#include "llvm/MC/MCELFStreamer.h"
+#include "llvm/MC/MCInstrAnalysis.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/MC/TargetRegistry.h"
+
+namespace llvm {
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializePICMidTarget() {}
+
+} // namespace llvm
