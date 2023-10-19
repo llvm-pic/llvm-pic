69990f8f87493a3e5a29faa903013a39cf315dc7
Thanh <quang.thanh.ta@pr1meti.de>
2023-10-19 07:02:29 +0200

    feat(PICMid/Target): Elaborate on instr and reg

--- llvm/lib/Target/PICMid/MCTargetDesc/PICMidMCTargetDesc.cpp
+++ llvm/lib/Target/PICMid/MCTargetDesc/PICMidMCTargetDesc.cpp
@@ -1,18 +1,44 @@
 #include "PICMidMCTargetDesc.h"
 #include "TargetInfo/PICMidTargetInfo.h"
 
+#include "llvm/ADT/ArrayRef.h"
 #include "llvm/BinaryFormat/ELF.h"
 #include "llvm/MC/MCAsmBackend.h"
 #include "llvm/MC/MCCodeEmitter.h"
 #include "llvm/MC/MCELFStreamer.h"
 #include "llvm/MC/MCInstrAnalysis.h"
-#include "llvm/MC/MCInstrInfo.h"
 #include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/MC/TargetRegistry.h"
 
+#define GET_INSTRINFO_MC_DESC
+#include "PICMidGenInstrInfo.inc"
+
+// #define GET_SUBTARGETINFO_MC_DESC
+// #include "PICMidGenSubtargetInfo.inc"
+
+#define GET_REGINFO_MC_DESC
+#include "PICMidGenRegisterInfo.inc"
+
 namespace llvm {
 
+// Internally used functions
+static MCRegisterInfo *createPICMidMCRegisterInfo(const Triple &tt) {
+  MCRegisterInfo *X = new MCRegisterInfo();
+  InitPICMidMCRegisterInfo(X, 0);
+  return X;
+}
+
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializePICMidTargetMC() {}
+
+MCInstrInfo *createPICMidMCInstrInfo() {
+  MCInstrInfo *X = new MCInstrInfo();
+  InitPICMidMCInstrInfo(X);
+
+  TargetRegistry::RegisterMCInstrInfo(getThePICMidTarget(), createPICMidMCInstrInfo);
+  TargetRegistry::RegisterMCRegInfo(getThePICMidTarget(), createPICMidMCRegisterInfo);
+
+  return X;
+}
 
 } // namespace llvm
--- llvm/lib/Target/PICMid/MCTargetDesc/PICMidMCTargetDesc.h
+++ llvm/lib/Target/PICMid/MCTargetDesc/PICMidMCTargetDesc.h
@@ -13,8 +13,56 @@
 #ifndef LLVM_PICMID_MCTARGET_DESC_H
 #define LLVM_PICMID_MCTARGET_DESC_H
 
+#include <memory>
+
 #include "llvm/ADT/Sequence.h"
+#include "llvm/MC/MCCodeEmitter.h"
+#include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCInstrDesc.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCObjectWriter.h"
 #include "llvm/Support/DataTypes.h"
+
+#include "TargetInfo/PICMidTargetInfo.h"
+
+namespace llvm {
+MCInstrInfo *createPICMidMCInstrInfo();
+MCCodeEmitter *createPICMidMCCodeEmitter(const MCInstrInfo &mcii,
+                                         MCContext &ctx);
+MCAsmBackend *createPICMidMCAsmBackend(const Target &t,
+                                       const MCSubtargetInfo &sti,
+                                       const MCRegisterInfo &mri,
+                                       const MCTargetOptions &to);
+std::unique_ptr<MCObjectTargetWriter>
+createPICMidELFObjectWriter(uint8_t osabi);
+} // namespace llvm
+
+#define GET_REGINFO_ENUM
+#include "PICMidGenRegisterInfo.inc"
+
+#define GET_INSTRINFO_ENUM
+#include "PICMidGenInstrInfo.inc"
+
+#define GET_SUBTARGETINFO_ENUM
+#include "PICMidGenSubtargetInfo.inc"
+
+namespace llvm {
+template <> struct enum_iteration_traits<decltype(PICMid::NoRegister)> {
+  static constexpr bool is_iterable = true;
+};
+
+namespace PICMidOp {
+
+enum OperandType : unsigned {
+    OPERAND_IMM8 = MCOI::OPERAND_FIRST_TARGET,
+    OPERAND_IMM1,
+    OPERAND_IMM3,
+    OPERAND_PCABS11,
+    OPERAND_ADDR7,
+};
+
+} // namespace PICMidOp
+
+} // namespace llvm
 
 #endif // LLVM_PICMID_MCTARGET_DESC_H
