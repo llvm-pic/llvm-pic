261c4b0de2311ee40fb77e503801810651461115
Hannes Diener <hannes.diener@pm.me>
2024-04-07 15:08:30 +0200

    picmid clang toolchain enters clang_main

---
+++ clang/lib/Driver/ToolChains/PICmid.h
@@ -0,0 +1,80 @@
+// part of PICmid
+// author: hd
+
+#ifndef CLANG_LIB_DRIVER_TOOLCHAINS_PICMID_H_
+#define CLANG_LIB_DRIVER_TOOLCHAINS_PICMID_H_
+
+#include "clang/Driver/Tool.h"
+#include "clang/Driver/ToolChain.h"
+
+#include "llvm/Support/Compiler.h"
+
+namespace clang {
+namespace driver {
+
+namespace toolchains {
+
+class LLVM_LIBRARY_VISIBILITY PICmidToolChain : public ToolChain {
+public:
+  PICmidToolChain(const Driver &D, const llvm::Triple &Triple,
+      const llvm::opt::ArgList &Args);
+
+protected:
+  Tool *buildLinker() const override;
+
+public:
+    // Position independet Code / Executable
+    bool isPICDefault() const override { return false; }
+    bool isPIEDefault(const llvm::opt::ArgList &Args) const override {
+        return false;
+    }
+    bool isPICDefaultForced() const override { return true; }
+
+    bool HasNativeLLVMSupport() const override { return true; }
+    bool IsIntegratedAssemblerDefault() const override { return true; }
+
+    const char *getDefaultLinker() const override { return "ld.lld"; }
+    RuntimeLibType GetDefaultRuntimeLibType() const override {
+        // for options see ToolChain.h; other option would be libgcc
+        return ToolChain::RLT_CompilerRT;
+    }
+
+    bool SupportsProfiling() const override { return false; }
+
+    void AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
+        llvm::opt::ArgStringList &CC1Args) const override;
+    void addClangTargetOptions(const llvm::opt::ArgList &DriverArgs,
+        llvm::opt::ArgStringList &CC1Args,
+        Action::OffloadKind DeviceOffloadKind) const override;
+};
+
+} // namespace toolchains
+
+namespace tools {
+namespace picmid {
+
+class LLVM_LIBRARY_VISIBILITY Linker : public Tool {
+public:
+  Linker(const ToolChain &TC) : Tool("PICmid::Linker", "ld.lld", TC) {}
+  bool isLinkJob() const override { return true; }
+  bool hasIntegratedCPP() const override { return false; }
+  void ConstructJob(Compilation &C, const JobAction &JA,
+    const InputInfo &Output, const InputInfoList &Inputs,
+    const llvm::opt::ArgList &TCArgs,
+    const char *LinkingOutput) const override;
+
+private:
+    // Link Time Optimization
+    void AddLTOOptions(const toolchains::PICmidToolChain &TC, const llvm::opt::ArgList &Args,
+        const InputInfo &Output, const InputInfoList &Inputs,
+        llvm::opt::ArgStringList &CmdArgs) const;
+};
+
+} // namespace picmid
+} // namespace tools
+
+} // namespace driver
+} // namespace clang
+
+#endif // not CLANG_LIB_DRIVER_TOOLCHAINS_PICMID_H_
+
